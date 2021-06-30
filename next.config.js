0249ee87a6109962fea69e410371a4dcb9d28a42
huug buug <yondooo61@gmail.com>
2021-06-30 21:09:13 +0800

    dd

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
     env: {
         // serverUrl: 'http://localhost:1337',
         serverUrl: 'http://localhost:1337',
-        frontUrl: 'http://localhost:3000',
-        // frontUrl: 'https://infosystems.mn',
+        // frontUrl: 'http://localhost:3000',
+        frontUrl: 'https://infosystems.mn',
         pageUrl: '/p/',
         postUrl: '/posts/',
         productUrl: '/product/',
