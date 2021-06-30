41651e4eca687e305dc05f4484664e443420aadf
huug buug <yondooo61@gmail.com>
2021-06-30 11:06:49 +0800

    dd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     env: {
-        // serverUrl: 'https://emedee-admin.herokuapp.com',
+        // serverUrl: 'http://localhost:1337',
         serverUrl: 'http://localhost:1337',
-        // serverUrl: 'http://192.168.0.105:1338',
-        frontUrl: 'http://localhost:3000',
+        // frontUrl: 'http://localhost:3000',
+        frontUrl: 'https://infosystems.mn',
         pageUrl: '/p/',
         postUrl: '/posts/',
         productUrl: '/product/',
