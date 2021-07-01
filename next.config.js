ef8bc16376546fa878bb65c58d00a683427a6b52
infosystems-mn <contact@infosystems.mn>
2021-07-01 15:26:34 +0800

    server change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    env: {
-        // serverUrl: 'http://localhost:1337',
-        serverUrl: 'http://localhost:1337',
-        // frontUrl: 'http://localhost:3000',
-        frontUrl: 'https://infosystems.mn',
-        pageUrl: '/p/',
-        postUrl: '/posts/',
-        productUrl: '/product/',
-    }
-}
+  env: {
+    // serverUrl: 'http://localhost:1337',
+    serverUrl: "http://188.166.188.164/:1337",
+    // frontUrl: 'http://localhost:3000',
+    frontUrl: "https://infosystems.mn",
+    pageUrl: "/p/",
+    postUrl: "/posts/",
+    productUrl: "/product/",
+  },
+};
