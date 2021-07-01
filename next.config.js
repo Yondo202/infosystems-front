74967f49ed08a4afe8ac9c024500553cafe74111
infosystems-mn <contact@infosystems.mn>
2021-07-01 15:40:57 +0800

    dahiad

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     // serverUrl: 'http://localhost:1337',
-    serverUrl: "http://188.166.188.164/:1337",
+    serverUrl: "http://188.166.188.164:1337",
     // frontUrl: 'http://localhost:3000',
     frontUrl: "https://infosystems.mn",
     pageUrl: "/p/",
