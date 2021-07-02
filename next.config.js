5e1c2871f598562427f799ace9e88365b130fbf1
infosystems-mn <contact@infosystems.mn>
2021-07-02 09:37:37 +0800

    url changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
     // serverUrl: 'http://localhost:1337',
-    serverUrl: "http://188.166.188.164:1337",
-    frontUrl: 'http://188.166.188.164:3000',
+    serverUrl: "https://infosystems.mn/api",
+    frontUrl: "https://infosystems.mn",
     // frontUrl: "https://infosystems.mn",
     pageUrl: "/p/",
     postUrl: "/posts/",
